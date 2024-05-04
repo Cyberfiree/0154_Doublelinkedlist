1844d6b5c98702ef1dcd33c61f6f0bf838563bdb
Cyberfire <buayagoreng99@gmail.com>
2024-05-04 21:57:36 +0800

    membuat pointer yang menunjuk awal / start linked list

--- doublelinkedlist/doublelinkedlist.cpp
+++ doublelinkedlist/doublelinkedlist.cpp
@@ -5,7 +5,12 @@ using namespace std;
 // Membuat struktur untuk menyatukan banyak data
 struct Node
 {
+	int noMhs; //Nomor mahasiswa
+	string name; // nama mahasiswa
+	Node* next; // pointer ke node berikutnya
+	Node* prev; // pointer ke node sebelumnya
 };
 
+Node* START = NULL; // pointer global yang menunjuk atau berada di awal / start linked list
 int main() {
 }
